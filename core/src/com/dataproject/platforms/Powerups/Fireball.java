cb48d289e84355f1beeed151b24e1dc6aa746de1
Luka <mr.bagel1@gmail.com>
2018-12-15 14:39:32 -0500

    - Added light to center of screen - ADDED NEW LIBRARY: LIQUID FUN (for waves, box2d extension)

--- core/src/com/dataproject/platforms/Powerups/Fireball.java
+++ core/src/com/dataproject/platforms/Powerups/Fireball.java
@@ -1,6 +1,48 @@
 package com.dataproject.platforms.Powerups;
 
-public class Fireball {
-    int k = 234234;
+import com.dataproject.platforms.Player;
+import com.dataproject.platforms.Utilities.Range;
+
+public class Fireball implements Powerup
+{
+    @Override
+    public void setRarity(double p) {
+
+    }
+
+    @Override
+    public void setChanceToHarmUser(double p) {
+
+    }
+
+    @Override
+    public double getRarity() {
+        return 0;
+    }
+
+    @Override
+    public double getChanceToHarmUser() {
+        return 0;
+    }
+
+    @Override
+    public void animate(float dt) {
+
+    }
+
+    @Override
+    public void initTextures() {
+
+    }
+
+    @Override
+    public Range getRange() {
+        return null;
+    }
+
+    @Override
+    public void use(Player self, Player other, boolean harmSelf) {
+
+    }
     // Luka is gay
 }
